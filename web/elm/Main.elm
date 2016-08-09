db4d66d53fda0a601770597cd87139165f57d312
Most <heartmoar@gmail.com>
2016-08-09 04:19:55 +0000

    Begin setting up submission port for reflections

--- web/elm/Main.elm
+++ web/elm/Main.elm
@@ -1,9 +1,11 @@
 port module Main exposing (..)
 import Html exposing (text, Html)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 import JSPhoenix exposing (ChannelEventMsg, ChanExitCB)
 import Markdown exposing (..)
-import Json.Decode exposing (int, string, bool)
-import Json.Encode exposing (null)
+import Json.Decode as JD exposing (int, string, bool)
+import Json.Encode as JE exposing (int, null)
 import Html.App as Html
 import Html exposing (..)
 
@@ -41,7 +43,7 @@ type alias RoomSyncState =
 
 type alias RoomSyncEvent =
   { id : String
-  , old : Maybe { String, List RoomSyncMeta }
+  , old : Maybe ( String, List RoomSyncMeta )
   , new : List ( String, List RoomSyncMeta )
   }
 
@@ -68,7 +70,7 @@ port onRoomSyncJoin : (ChannelEventMsg RoomSyncEvent Int -> msg) -> Sub msg
 port onRoomSyncLeave : (ChannelEventMsg RoomSyncEvent Int -> msg) -> Sub msg
 
 port portSubmit: Model -> Cmd msg
-port portReflection: (ChannelEventMsg RoomMessage Model -> msg) -> Sub msg
+port portReflection: (Model -> msg) -> Sub msg
 
 
 connect_room rid =
@@ -77,28 +79,64 @@ connect_room rid =
       , timeout_ms = Nothing -- Just 10000 -- Default value is 10000 if Nothing is used
       , chanCloseCB = Nothing
       , chanErrorCB = Nothing
-      , syncState = Just { portName = "onRoomSyncState", cb_data = (int rid) }
-      , syncJoin = Just { portName = "onRoomSyncJoin", cb_data = (int rid) }
-      , syncLeave = Just { portName = "onRoomSyncLeave", cb_data = (int rid) }
+      , syncState = Just { portName = "onRoomSyncState", cb_data = (JE.int rid) }
+      , syncJoin = Just { portName = "onRoomSyncJoin", cb_data = (JE.int rid) }
+      , syncLeave = Just { portName = "onRoomSyncLeave", cb_data = (JE.int rid) }
       , joinData = null
       , joinEvents =
-          [ { portName = "onRoomConnect", msgID = "ok", cb_data = (int rid) }
+          [ { portName = "onRoomConnect", msgID = "ok", cb_data = (JE.int rid) }
           ]
       , onPorts =
-          [ { portName = "onRoomInfo", msgID = "room:info", cb_data = (int rid) }
-          , { portName = "onRoomMsgsInit", msgID = "msgs:init", cb_data = (int rid) }
-          , { portName = "onRoomMsgsAdd", msgID = "msgs:add", cb_data = (int rid) }
+          [ { portName = "onRoomInfo", msgID = "room:info", cb_data = (JE.int rid) }
+          , { portName = "onRoomMsgsInit", msgID = "msgs:init", cb_data = (JE.int rid) }
+          , { portName = "onRoomMsgsAdd", msgID = "msgs:add", cb_data = (JE.int rid) }
           ]
       }
 
+type alias ModelFormatted = 
+    { cb_data : Model
+    , msg : RoomMessage
+    , msgID : String
+    , topic : String
+    }
+
 type Msg
-    = SubmitReflection Model
+    = NoOp
+    | SubmitReflection
+    | InitRefl Model
+    | ReflectionDate String
+    | ReflectionAuthor String
+    | ReflectionText String
+    | Published Bool
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
-    SubmitReflection _ ->
-        ( model, Cmd.none )
+    NoOp -> (model, Cmd.none)
+    
+    InitRefl newModel -> 
+        let
+          foo = Debug.log "INIT REFL" newModel
+        in
+          ( newModel , Cmd.none)
+    ReflectionDate s ->
+      ( { model | date = s }, Cmd.none)
+
+    ReflectionAuthor s -> 
+      ( { model | author = s }, Cmd.none)
+
+    ReflectionText s ->
+      ( { model | text = s }, Cmd.none)
+      
+    SubmitReflection ->
+        ( model, portSubmit model )
+        
+    Published bool ->
+      let
+        foo = Debug.log "PUBLISHED" bool
+        newModel = { model | published = bool }
+      in
+        ( newModel, portSubmit newModel)
 --     MyConnectMessage rid ->
 --       ( model
 --       , connect_room rid -- You can use the JSPhoenix.connect like any normal command
@@ -117,16 +155,91 @@ update msg model =
 --     , onRoomSyncLeave MyRoomSyncLeaveMsg
 --     ]
 
-
-view : Model -> Html Msg
-view = 
-    p [] [ text "Hello Foo" ]
-  
+subscriptions: Model -> Sub Msg
+subscriptions model =
+  Sub.batch
+  [ portReflection InitRefl]
 
 main = Html.program
   { init = init
   , update = update
   , view = view
-  , subscriptions = (\model -> portReflection SubmitReflection)
+  , subscriptions = subscriptions
   }
  
+ 
+ 
+ 
+ -- View
+ 
+view : Model -> Html Msg
+view model =
+  div [ id "refl-edit"]
+  [ inputDate model
+  , inputAuthor model
+  , inputText model
+  , ul [id "refl-buttons"]
+      [ li [] [ button [ onClick SubmitReflection ] [ text "Submit" ] ] 
+      ]
+  , showText model
+  ]
+
+inputDate: Model -> Html Msg
+inputDate model =
+  p [] 
+    [ input
+        [ id "reflection-date"
+        , name "reflection-date"
+        , type' "text"
+        , placeholder "Reflection Date"
+        , Html.Attributes.value model.date
+        , onInput ReflectionDate
+        , autofocus True
+        ] 
+        []
+    ]
+
+inputAuthor: Model -> Html Msg
+inputAuthor model =
+  p [] 
+    [ input
+        [ id "reflection-author"
+        , name "reflection-author"
+        , type' "text"
+        , placeholder "Your name"
+        , Html.Attributes.value model.author
+        , onInput ReflectionAuthor
+        , autofocus True
+        ] 
+        []
+    ]
+
+inputText: Model -> Html Msg
+inputText model =
+  p [ id "refl-textarea"]
+    [ textarea 
+        [ id "reflection-text"
+        , name "reflection-text"
+        , placeholder "Use Markdown formatting"
+        , Html.Attributes.value model.text
+        , onInput ReflectionText
+        , autofocus True
+        ]
+        []
+    ]
+
+showText: Model -> Html Msg
+showText model =
+  div [id "preview"] 
+  [ h3 [] 
+    [ text "Preview  "
+    , span [ style [("font-weight", "normal"), ("font-size", "0.7em")] ] [ text "( published" ]
+    , input [ type' "checkbox"
+            , checked model.published
+            , onCheck Published
+            ]
+            []
+    , span [ style [("font-weight", "normal"), ("font-size", "0.7em")] ] [ text ")" ]
+    ]
+  , div [id "md-text"] [Markdown.toHtml [] model.text]
+  ]
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -15,8 +15,9 @@ socket.onOpen(() => {
     let app = Elm.Main.embed(document.querySelector('#elm-target'))
     setup_phoenix_socket_ports(app, socket)
     
-    app.ports.portReflection.send(true);
+    
     app.ports.portSubmit.subscribe(function (val) {
+        app.ports.portReflection.send(val);
         console.log(val);
     });
   });
